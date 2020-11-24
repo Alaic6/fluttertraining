e78a8b968d9a5d796796e273a842a7f7aa638f6f
Alaic6 <truongalexis@gmail.com>
2020-11-24 06:16:18 +0100

    Avancee dans le code
    Signed-off-by: Alaic6 <truongalexis@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -21,9 +23,32 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: crossAxisAlignment.start,
-        children: [],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(width: 50, height: 50),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                IconButton(icon: Icon(Icons.notifications),onPressed: () {}),
+                IconButton(icon: Icon(Icons.extension),onPressed: () {}),
+              ],),
+            SizedBox(height: 50),
+            Text.rich(
+              TextSpan(
+                text: 'Bienvenue,',
+                style:TextStyle(fontWeight: FontWeight.bold),
+                children: [
+                  TextSpan(
+                    text: ' Alexis',
+                  )
+                ]),
+            style: TextStyle(fontSize: 90),
+            )
+          ],
+        ),
       ),
     );
   }
